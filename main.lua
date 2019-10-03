321b8ad2700c6f594b8b767a75814436900b0728
Josh Waggoner <joshwaggoner@gmail.com>
2019-10-03 03:46:25 -0500

    added second line for xp gains required to level localized functions update toc version 0.2

--- main.lua
+++ main.lua
@@ -1,10 +1,14 @@
 
+PetXP = { }
+
 local currXP, nextXP = GetPetExperience()
+local prevXP = nil
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("UNIT_PET_EXPERIENCE")
 f:SetScript("OnEvent", function(self, event)
-	C_Timer.After(2,updatePetXP)
+	--C_Timer.After(2,updatePetXP)
+	PetXP:updatePetXP(false)
 end)
 
 local f2 = CreateFrame("Frame")
@@ -12,9 +16,9 @@ f2:RegisterEvent("PET_BAR_UPDATE")
 f2:RegisterEvent("UNIT_PET")
 f2:SetScript("OnEvent", function(self, event)
 	if event == "UNIT_PET" then
-		updatePetXP()
+		PetXP:updatePetXP(true)
 	end
-	hunterPetActive()
+	PetXP:hunterPetActive()
 end)
 
 local frame = CreateFrame("Frame", "DragFrame2", UIParent)
@@ -23,39 +27,48 @@ frame:EnableMouse(true)
 frame:RegisterForDrag("LeftButton")
 frame:SetScript("OnDragStart", frame.StartMoving)
 frame:SetScript("OnDragStop", frame.StopMovingOrSizing)
-frame.text = frame:CreateFontString(nil,"ARTWORK") 
-frame.text:SetFont("Fonts\\ARIALN.ttf", 13, "OUTLINE")
-frame.text:SetPoint("LEFT",0,0)
-
--- The code below makes the frame visible, and is not necessary to enable dragging.
 frame:SetPoint("CENTER")
 frame:SetWidth(100)
 frame:SetHeight(10)
+frame.text = frame:CreateFontString(nil,"ARTWORK") 
+frame.text:SetFont("Fonts\\ARIALN.ttf", 13, "OUTLINE")
+frame.text:SetPoint("TOPLEFT")
+frame.text:SetJustifyH("LEFT")
 
 
-function printPetXP ()
+
+
+function PetXP:printPetXP (prevXP)
 	local percent = string.format("%2.2f", (currXP / nextXP) * 100)
 	local level = UnitLevel("pet")
 	local text = "PetXP: " .. currXP .. " / " .. nextXP .. " (Lvl " .. level .. " - " .. percent .. "%)"
-	--print(text)
+	if prevXP then
+		local gains = math.ceil((nextXP - currXP) / (currXP - prevXP))
+		text = text .. "\nXP gains req: " .. gains
+	end
 	frame.text:SetText(text)
 end
 
-function updatePetXP ()
+function PetXP:updatePetXP (init)
+	if not init then
+		prevXP = currXP
+	else 
+		prevXP = nil
+	end
 	currXP, nextXP = GetPetExperience()
-	printPetXP()
+	PetXP:printPetXP(prevXP)
 end
 
-function hunterPetActive ()
+function PetXP:hunterPetActive ()
 	local hasUI, isHunterPet = HasPetUI();
 	frame:Hide()
 	if hasUI then
 		if isHunterPet then
-			printPetXP()
+			PetXP:printPetXP(prevXP)
 			frame:Show()
 		end
 	end
 end
 
-printPetXP()
-hunterPetActive()
+PetXP:printPetXP()
+PetXP:hunterPetActive()
